35cc9d688146aa2fb49e35f3da97493775ece4b4
kevinpicon01 <digitalpruu@gmail.com>
2024-10-01 01:22:24 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,9 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Visión 20/20";
+const email = "anaisabelbenavides79@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    `¡Hola! Estoy interesado en los productos de ${title} y me gustaría recibir más información sobre gafas y accesorios que ofrecen. ¡Espero su respuesta!`
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "3227087758";
 const textos = {
     index: {
         title: title,
@@ -12,25 +12,24 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "*Visión 20/20* se dedica a ofrecer una amplia gama de productos para el cuidado de la visión, incluyendo gafas de aumento, estuches para gafas, cordones para gafas y más. Nuestro objetivo es proporcionar soluciones ópticas que mejoren tu visión y se adapten a tus necesidades diarias.",
+        description2: "Contamos con una variada selección de gafas para cerca y lejos, diseñadas para ofrecer comodidad y estilo, además de estuches y accesorios que protegen y complementan tus gafas.",
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Vision 20/20",
+            p2: "Tu visión es nuestra prioridad",
         },
-        address: "Dirección del emprendimiento",
+        address: "",
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        facebook: "https://www.facebook.com/share/u2ZqpxbuDyyoExws/?mibextid=qi2Omg",
+        instagram: "https://www.instragram.com",
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
 export default textos;
